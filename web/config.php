1a6d5b487e3ec6e60f36a8e6113eb7198a948935
yejianjian <yejianjian4816@gmail.com>
2018-02-16 14:15:36 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyDJRh2Qlbw8sG23Ipb59Isc61D602JQ0U8');
+define('GJ_CODE', 'HK');
+define('SITE_NAME','mytube');
+define('TITLENAME', 'Mytube');
+define('EN2DEKEY','aQaHo6IFyj9hZH1YEvK2');
+define('EMAIL', 'yejianjian4816@gmail.com');
 ?>
