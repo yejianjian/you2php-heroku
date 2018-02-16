32fc18c481d6c84d7103338ca0822aacc787a1c5
yejianjian <yejianjian4816@gmail.com>
2018-02-16 16:04:43 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDJRh2Qlbw8sG23Ipb59Isc61D602JQ0U8');
-define('GJ_CODE', 'HK');
+define('GJ_CODE', 'US');
 define('SITE_NAME','mytube');
 define('TITLENAME', 'Mytube');
 define('EN2DEKEY','aQaHo6IFyj9hZH1YEvK2');
